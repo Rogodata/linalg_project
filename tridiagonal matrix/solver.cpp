7abf3324a65179b2737390940dc9cf341c43d045
Rogodata <shumov.ai@phystech.edu>
2023-02-10 20:41:48 +0300

    it solves basic test

--- tridiagonal matrix/solver.cpp
+++ tridiagonal matrix/solver.cpp
@@ -8,7 +8,7 @@ struct Row
 {
     Row()
     {
-        //default constructor
+        // default constructor
         a = float(0);
         b = float(0);
         c = float(0);
@@ -53,7 +53,6 @@ public:
         {
             file_stream >> a_number >> b_number >> c_number;
             rows.push_back(Row(a_number, b_number, c_number));
-            std::cout << rows[i].a << rows[i].b << rows[i].c << std::endl;
         }
         file_stream >> a_number >> b_number;
         rows.push_back(Row(a_number, b_number, float(0)));
@@ -63,8 +62,9 @@ public:
     std::vector<T> get_solution(T *answers_array)
     {
         // Не очень здорово, что я просто подаю указатель на массив данных, но думаю, так быстрее чем с вектором
-        //К тому, предполагается, что я доверяю поданному столбцу свободных членов
+        // К тому, предполагается, что я доверяю поданному столбцу свободных членов
         std::vector<T> result_array;
+        result_array.resize(n_parameter + 1);
         T *p = new T[n_parameter + 1];
         T *q = new T[n_parameter + 1];
         // прямой ход
@@ -72,8 +72,8 @@ public:
         q[1] = answers_array[0] / rows[0].b;
         for (long i = 1; i < n_parameter; i++)
         {
-            p[i + 1] = -(rows[i].c / (rows[i].a * p[i] + rows[i].b));
-            q[i + 1] = -((answers_array[i] - rows[i].a * q[i]) / (rows[i].a * p[i] + rows[i].b));
+            p[i + 1] = -((rows[i].c) / (rows[i].a * p[i] + rows[i].b));
+            q[i + 1] = ((answers_array[i] - rows[i].a * q[i]) / (rows[i].a * p[i] + rows[i].b));
         }
         // обратный ход
         result_array[n_parameter] = (answers_array[n_parameter] - rows[n_parameter].a * q[n_parameter]) / (rows[n_parameter].a * p[n_parameter] + rows[n_parameter].b);
@@ -86,21 +86,36 @@ public:
         return result_array;
     }
 
-    //вектор строк надо будет убрать в private
-    std::vector<Row<T>> rows;
+    friend std::ostream &operator<<(std::ostream &os, const TridiagonalMatrix<T> &matrix)
+    {   
+        os << "The values of diagonals are:" << std::endl;
+        for (unsigned int i = 0; i < matrix.n_parameter + 1; i++)
+        {
+            os << matrix.rows[i].a << " " << matrix.rows[i].b << " " << matrix.rows[i].c << std::endl;
+        }
+        os << "the end of matrix";
+        return os;
+    };
 
 private:
     // поле n_parameter здесь на 1 меньше реального размера матрицы. Сделано для удобства отсчёта элементов с нуля.
     unsigned int n_parameter;
+    std::vector<Row<T>> rows;
 };
 
 int main()
 {
     TridiagonalMatrix test_matrix = TridiagonalMatrix<float>(4);
     test_matrix.read_data("test_1.txt");
-    for (int i = 0; i < 4; i++)
+    // выведем матрицу
+    std::cout << test_matrix << std::endl;
+    float d_array[4] = {1, 15, 7, 1};
+    // решаем
+    std::vector<float> result = test_matrix.get_solution(d_array);
+    // выводим решение
+    for (auto it = result.begin(); it != result.end(); it++)
     {
-        std::cout << test_matrix.rows[i].a << " " << test_matrix.rows[i].b << " " << test_matrix.rows[i].c << std::endl;
+        std::cout << *it << std::endl;
     }
     return 0;
 }
