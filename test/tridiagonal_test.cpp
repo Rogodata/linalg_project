f432bd74da315ae4663fc19fee7a043589edcded
Rogodata <shumov.ai@phystech.edu>
2023-02-19 19:29:35 +0300

    Made gtest working

---
+++ test/tridiagonal_test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include "../source/tridiagonal_solver.hpp"
+
+
+
+using namespace std;
+
+TEST(TestGroupName, Subtest_1) {
+  ASSERT_TRUE(1 == 1);
+}
+
+TEST(TestGroupName, Subtest_2) {
+  ASSERT_FALSE('b' == 'b');
+  cout << "continue test after failure" << endl;
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
